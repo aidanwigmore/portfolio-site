b6523077a62657e4fb4f0051b64a3c75751fe2e6
aidanwigmore <aidanwigmore4@gmail.com>
2026-03-17 17:17:11 -0400

    Pagination on Demos Page

--- frontend/src/pages/Videos.tsx
+++ frontend/src/pages/Videos.tsx
@@ -5,15 +5,17 @@ import ListItem from '@mui/material/ListItem';
 import ListItemAvatar from '@mui/material/ListItemAvatar';
 import ListItemText from '@mui/material/ListItemText';
 
-import { Pagination, AccordionDetails, AccordionSummary, Box, Button, Step, StepLabel, Stepper, Tooltip } from '@mui/material';
+import { Pagination, AccordionDetails, AccordionSummary, Box, Button, Step, StepLabel, Stepper, Tooltip, Typography, Tab } from '@mui/material';
 import Accordion from '@mui/material/Accordion';
 import YoutubeIcon from '@mui/icons-material/YouTube';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
 import { CustomTypography } from '../materials/Typography';
+import TabContext from '@mui/lab/TabContext';
+import TabList from '@mui/lab/TabList';
+import TabPanel from '@mui/lab/TabPanel';
 
 export default function Contact() {
-
   const items= [
     {
       title: "Batched Support",
@@ -25,9 +27,9 @@ export default function Contact() {
         "https://www.youtube.com/embed/lQwYJoPhWwQ?si=WmTaPeI0Ib-7s82c",
       ],
       steps: [
-        'Pickup a catered product from strathcona mrkt', 
-        'How to subscribe to mrktbox', 
-        'Sign Up To MRKTBOX'
+        'Picking Up Catering Orders from Strathcona MRKT', 
+        'Subscribing To MRKTBox', 
+        'Signing Up To MRKTBox'
       ],
       stepsLabels: [
         (  
@@ -81,8 +83,8 @@ export default function Contact() {
       ],
     },
     {
-      title: "Mohawk College",
-      description: "TeaBank Capstone Final Demonstration",
+      title: "Mohawk College Capstone",
+      description: "TeaBank Final Demonstrations",
       channel: "aidanOdyssey",
       src: [
         "https://www.youtube.com/embed/fVJ_0QYCSoI?si=p7jX3Wn7ylXdaZxE",
@@ -332,11 +334,15 @@ export default function Contact() {
       ],
     }, 
     {
-      title: "Mohawk College",
-      description: "PHP Demonstrations",
+      title: "PHP / Laravel",
+      description: "Mohawk College PHP Course Demonstrations",
       channel: "aidanOdyssey",
-      src: ["https://www.youtube.com/embed/videoseries?list=PLh17cpqc5tdWh_b2x-6Am0r08BeYy0H2g"],
-      steps: ['Step 1', 'Step 2', 'Step 3'],
+      src: [
+        "https://www.youtube.com/embed/enO66A4MEjA?si=hljKuYuVjIETDRgi",
+        "https://www.youtube.com/embed/otVmSjKBpyQ?si=2JoxDWcCQPJFrSVl",
+        "https://www.youtube.com/embed/XFhhSQ-mEgM?si=-UdqBiqhLYOWUhhK",
+      ],
+      steps: ['Assignment 5', 'Assignment 6','Assignment 7'],
       stepsLabels: [
         (
           <CustomTypography component="span">
@@ -356,8 +362,8 @@ export default function Contact() {
       ],
     },
     {
-      title: "Mohawk College",
-      description: "Mohawk College Technical Presentations",
+      title: "Soft Skills",
+      description: "Mohawk College Technical / Entrepeneurship Presentations",
       channel: "aidanOdyssey",
       src: ["https://www.youtube.com/embed/videoseries?list=PLh17cpqc5tdUla07BUe_gP5kNb0sowuh_"],
       steps: ['Step 1', 'Step 2', 'Step 3'],
@@ -381,7 +387,7 @@ export default function Contact() {
     },
   ];
 
-  const [currentPages, setCurrentPages] = useState<number[]>(items.map(() => 1));
+  const [currentPage, setCurrentPage] = useState<number>(1);
 
   const youtubeWidth = window.innerWidth > 600 ? "50%" : "100%";
   const youtubeHeight = window.innerWidth > 600 ? "50vh" : "30vh";
@@ -391,6 +397,12 @@ export default function Contact() {
   const [activeSteps, setActiveSteps] = useState<number[]>(items.map(() => 0));
   const [skippedSteps, setSkippedSteps] = useState<Set<number>[]>(items.map(() => new Set()));
   
+  const [value, setValue] = useState<number>(1);
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    setCurrentPage(newValue);
+    setValue(newValue);
+  };
+
   const isStepOptional = (step: number) => {
     return step === 1;
   };
@@ -468,10 +480,11 @@ export default function Contact() {
   };
 
   const handlePageChange = (itemIndex: number, pageNumber: number) => {
-    setCurrentPages((prev) => {
-      const newPages = [...prev];
-      newPages[itemIndex] = pageNumber;
-      return newPages;
+    setCurrentPage(pageNumber);
+    setActiveSteps((prev) => {
+      const newSteps = [...prev];
+      newSteps[itemIndex] = 0;
+      return newSteps;
     });
   };
   
@@ -487,148 +500,163 @@ export default function Contact() {
 
   return (
       <>
-      <Box sx={{ display: 'flex', justifyContent: 'center', mt: 2, mb: 2 }}>
-        <Pagination
-          count={items.length}
-          page={items.length > 0 ? currentPages[0] : 1}
-          onChange={(event, pageNumber) => {
-            setActiveSteps((prev) => {
-              const newSteps = [...prev];
-              newSteps[0] = 0;
-              return newSteps;
-            });
-            setElapsedTimes((prev) => {
-              const newTimes = [...prev];
-              newTimes[0] = 0;
-              return newTimes;
-            });
-          }}
-          color="primary"
-        />
-      </Box>
-      <List dense={false}>
-      {items.map((item, index) => {
-        const currentStep = activeSteps[index];
-        const isCompleted = currentStep === item.steps.length;
-        const elapsedTime = elapsedTimes[index];
-        const currentPage = currentPages[index];
+      <TabContext value={value}>
+        <Box sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', marginBottom: 2 }}>
+          <Typography variant="caption" component="span" sx={{ ml: 2 }}>
+            {items[currentPage - 1].title} - {items[currentPage - 1].description}
+          </Typography>
+        </Box>
+        <List dense={false}>
+        <TabList onChange={handleChange} aria-label="lab API tabs example" sx={{ display: 'flex', justifyContent: 'center', marginBottom: 2 }}>
+          {items.map((item, index) => (
+            <Tab key={index} label={`${item.title}`} value={index + 1} />
+          ))}
+        </TabList>
+        {items
+          .filter((item) => items.indexOf(item) === currentPage - 1)
+          .map((item, index) => {
+            const itemIndex = items.indexOf(item);
+            const currentStep = activeSteps[itemIndex];
+            const isCompleted = currentStep === item.steps.length;
 
-        return (
-          <ListItem
-            key={index}
-            sx={{
-              backgroundColor: index % 2 === 0 ? "#f9f9f9" : "#eaeaea",
-              borderRadius: "8px",
-              marginBottom: "16px",
-            }}
-          >
-            <ListItemAvatar>
-              <YoutubeIcon sx={{ color: "#FF0000" }} />
-            </ListItemAvatar>
-            <ListItemText primary={item.title} secondary={item.description} />
-            <Box sx={{ width: '100%', justifyContent: 'center', alignItems: 'center', display: 'flex', flexDirection: 'column' }}>
-              <Stepper activeStep={currentStep}>
-                {item.steps.map((label, stepIndex) => {
-                  const stepProps: { completed?: boolean } = {};
+          return (
+            <ListItem
+              key={itemIndex}
+              sx={{
+                backgroundColor: itemIndex % 2 === 0 ? "#f9f9f9" : "#eaeaea",
+                borderRadius: "8px",
+                marginBottom: "16px",
+              }}
+            >
+              <Box sx={{ width: '100%', justifyContent: 'center', alignItems: 'center', display: 'flex', flexDirection: 'column' }}>
+                <Stepper activeStep={currentStep}>
+                  {item.steps.map((label, stepIndex) => {
+                    const stepProps: { completed?: boolean } = {};
 
-                  if (isStepSkipped(index, stepIndex)) {
-                    stepProps.completed = false;
-                  }
+                    if (isStepSkipped(itemIndex, stepIndex)) {
+                      stepProps.completed = false;
+                    }
 
-                  return (
-                    <Step key={`step-${index}-${stepIndex}`} {...stepProps}>
-                      <StepLabel>{label}</StepLabel>
-                    </Step>
-                  );
-                })}
-              </Stepper>
-              <>
-              {isCompleted ? (
-                  <React.Fragment>
-                    <CustomTypography sx={{ mt: 1, mb: 1 }}>
-                      You can find more videos on the {item.channel} Youtube Channel. Thank you.
-                    </CustomTypography>
-                    <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
-                      <Box sx={{ flex: '1 1 auto' }} />
-                      <Button onClick={() => handleReset(index)}>Reset</Button>
-                    </Box>
-                  </React.Fragment>
-                ) : (
-                  <React.Fragment>
-                    <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
-                      <Button
-                        color="inherit"
-                        disabled={currentStep === 0}
-                        onClick={() => handleBack(index)}
-                        sx={{ mr: 1 }}
-                      >
-                        Back
-                      </Button>
-                      <Box sx={{ flex: '1 1 auto' }} />
-                      {isStepOptional(currentStep) && (
+                    return (
+                      <Step key={`step-${itemIndex}-${stepIndex}`} {...stepProps}>
+                        <StepLabel>{label}</StepLabel>
+                      </Step>
+                    );
+                  })}
+                </Stepper>
+                <>
+                {isCompleted ? (
+                    <React.Fragment>
+                      <CustomTypography sx={{ mt: 1, mb: 1 }}>
+                        You can find more videos on the {item.channel} Youtube Channel. Thank you.
+                      </CustomTypography>
+                      <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
+                        <Box sx={{ flex: '1 1 auto' }} />
+                        <Button onClick={() => handleReset(itemIndex)}>Reset</Button>
+                      </Box>
+                    </React.Fragment>
+                  ) : (
+                    <React.Fragment>
+                      <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
                         <Button
                           color="inherit"
-                          onClick={() => handleSkip(index)}
+                          disabled={currentStep === 0}
+                          onClick={() => handleBack(itemIndex)}
                           sx={{ mr: 1 }}
                         >
-                          Skip
+                          Back
                         </Button>
-                      )}
-                      <Button onClick={() => handleNext(index)}>
-                        {currentStep === item.steps.length - 1
-                          ? 'Finish'
-                          : 'Next'}
-                      </Button>
-                    </Box>
-                  </React.Fragment>
-                )}
-              </>
-              <Box
-                sx={{
-                  display: "flex",
-                  justifyContent: "center",
-                  flexDirection: "column",
-                  alignItems: "center",
-                  width: youtubeWidth,
-                  height: { sm: '25vw' },
-                  marginY: 2,
-                }}
-              >
-                <iframe
-                  src={item.src[currentStep % item.src.length]}
-                  title="YouTube video player"
-                  style={{
-                    borderRadius: "8px",
-                    width: "100%",
-                    height: "100%",
+                        <Box sx={{ flex: '1 1 auto' }} />
+                        {isStepOptional(currentStep) && (
+                          <Button
+                            color="inherit"
+                            onClick={() => handleSkip(itemIndex)}
+                            sx={{ mr: 1 }}
+                          >
+                            Skip
+                          </Button>
+                        )}
+                        <Button onClick={() => handleNext(itemIndex)}>
+                          {currentStep === item.steps.length - 1
+                            ? 'Finish'
+                            : 'Next'}
+                        </Button>
+                      </Box>
+                    </React.Fragment>
+                  )}
+                </>
+                
+                <Box
+                  sx={{
+                    display: "flex",
+                    justifyContent: "center",
+                    flexDirection: "column",
+                    alignItems: "center",
+                    width: youtubeWidth,
+                    height: { sm: '25vw' },
+                    marginY: 2,
                   }}
-                  frameBorder="0"
-                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                  allowFullScreen
-                ></iframe>
-              </Box>
-              
-              {item.stepsLabels ? (
-                <Accordion sx={{ width: '100%' }}>
-                  <AccordionSummary
-                    expandIcon={<ExpandMoreIcon />}
-                    aria-controls="panel1-content"
-                  >
-                    <CustomTypography component="span">Click for Transcriptions</CustomTypography>
-                  </AccordionSummary>
-                  <AccordionDetails>
-                    {item.stepsLabels[currentStep]}
-                  </AccordionDetails>
-                </Accordion>
-              ) : (
-                ''
-              )}
-
-              </Box>
-
-              </ListItem>
-        );     })}
-      </List>
+                >
+                  <iframe
+                    src={item.src[currentStep % item.src.length]}
+                    title="YouTube video player"
+                    style={{
+                      borderRadius: "8px",
+                      width: "100%",
+                      height: "100%",
+                    }}
+                    frameBorder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowFullScreen
+                  ></iframe>
+                  {item.stepsLabels ? (
+                  <Accordion sx={{maxHeight: '200px', overflowY: 'auto' }}>
+                    <AccordionSummary
+                      expandIcon={<ExpandMoreIcon />}
+                      aria-controls="panel1-content"
+                    >
+                      <CustomTypography component="span">Click for Transcriptions</CustomTypography>
+                    </AccordionSummary>
+                    <AccordionDetails>
+                      {item.stepsLabels[currentStep]}
+                    </AccordionDetails>
+                  </Accordion>
+                ) : (
+                  ''
+                )}
+                </Box>
+                </Box>
+                </ListItem>
+          );     })}
+        </List>
+        
+        <Box sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', marginBottom: 2 }}>
+          
+          <Typography variant="caption" component="span" sx={{ ml: 2 }}>
+            {items[currentPage - 1].title} - {items[currentPage - 1].description}
+          </Typography>
+          
+          <Pagination
+            count={items.length}
+            page={currentPage}
+            onChange={(event, pageNumber) => {
+              setCurrentPage(pageNumber);
+              setActiveSteps((prev) => {
+                const newSteps = [...prev];
+                newSteps[0] = 0;
+                return newSteps;
+              });
+            }}
+            color="primary"
+          />
+          
+          <Typography variant="caption" component="span" sx={{ ml: 2 }}>
+            (Page {currentPage} of {items.length})
+          </Typography>
+        
+        </Box>
+      
+      </TabContext>
       </>
   );
 }
